8802429d4e76a95b1b599ddb7fc242fdaee3f422
Haojie Zhang <zhj3408237@gmail.com>
2023-04-12 15:39:59 -0400

    Added ExerciseListActivity.java for the exercise recyler view

---
+++ app/src/main/java/com/example/gymbuddy/ExerciseListActivity.java
@@ -0,0 +1,11 @@
+package com.example.gymbuddy;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ExerciseListActivity extends AppCompatActivity {
+    private final String[] EXERCISE_LIST = {
+            "Bench Press "
+    };
+
+    }
