1f7441ed483a6a49c799073461793ce1fec57b77
Jack Lutfy <lutfyjack123@gmail.com>
2023-04-18 22:39:46 -0400

    Added activities and menu visibility

--- app/src/main/java/com/example/gymbuddy/MainActivity.java
+++ app/src/main/java/com/example/gymbuddy/MainActivity.java
@@ -2,13 +2,47 @@ package com.example.gymbuddy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "GYM_BUDDY";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int menuId = item.getItemId();
+
+        if (menuId == R.id.info_menu) {
+            Intent infoIntent = new Intent(this, InfoActivity.class);
+            startActivity(infoIntent);
+            return true;
+        } else if (menuId == R.id.exercise_menu) {
+            Log.d(TAG, "Exercise menu clicked!");
+            Intent exerciseIntent = new Intent(this, ExerciseActivity.class);
+            startActivity(exerciseIntent);
+            return true;
+        } else if (menuId == R.id.equipment_menu) {
+            Log.d(TAG, "Equipment menu clicked!");
+            Intent equipmentIntent = new Intent(this, EquipmentActivity.class);
+            startActivity(equipmentIntent);
+            return true;
+        } else if (menuId == R.id.workouts_menu) {
+            Intent workoutIntent = new Intent(this, WorkoutActivity.class);
+            startActivity(workoutIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
