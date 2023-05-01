8a22d3bc33ae21018668ce094df8d9af0e6b8996
Jack Lutfy <lutfyjack123@gmail.com>
2023-05-01 17:25:00 -0400

    recycler view adapter

--- app/src/main/java/com/example/gymbuddy/ExerciseListAdapter.java
+++ app/src/main/java/com/example/gymbuddy/ExerciseListAdapter.java
@@ -4,9 +4,13 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class ExerciseListAdapter extends
         RecyclerView.Adapter<ExerciseListAdapter.ExerciseViewHolder> {
@@ -28,9 +32,51 @@ public class ExerciseListAdapter extends
     @Override
     public void onBindViewHolder(@NonNull ExerciseListAdapter.ExerciseViewHolder holder, int position) {
         ExerciseData exercise = mExerciseList[position];
-        holder.campusImageView.setImageResource(campus.getProfileImage());
-        holder.campusTitleView.setText(campus.getCampusName());
-        holder.campusPhoneView.setText(campus.getPhone());
-        holder.campusEmailView.setText(campus.getEmail());
+        holder.exerciseNameView.setText((exercise.getExerciseName()));
+        holder.exerciseImageView.setImageResource(exercise.getExercisePreview());
+        holder.exerciseDescView.setText(exercise.getExerciseName());
+        holder.exerciseMuscleView.setText(exercise.getExerciseMuscleGroup());
+        holder.exerciseIDView.setText(exercise.getExerciseID());
+    }
+    @Override
+    public int getItemCount() {
+        return mExerciseList.length;
+    }
+
+    class ExerciseViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public final TextView exerciseNameView;
+        public final ImageView exerciseImageView;
+        public final TextView exerciseDescView;
+        public final TextView exerciseMuscleView;
+        public final TextView exerciseIDView;
+        final ExerciseListAdapter mAdapter;
+
+        public ExerciseViewHolder(@NonNull View itemView, ExerciseListAdapter adapter) {
+            super(itemView);
+            exerciseNameView = itemView.findViewById(R.id.exercise_list_name_id);
+            exerciseImageView = itemView.findViewById(R.id.exercise_list_image_id);
+           exerciseDescView = itemView.findViewById(R.id.exercise_list_desc_id);
+           exerciseMuscleView = itemView.findViewById(R.id.exercise_list_muscle_id);
+           exerciseIDView = itemView.findViewById(R.id.exercise_list_id_id);
+            this.mAdapter = adapter;
+
+            exerciseNameView.setOnClickListener(this);
+           exerciseImageView.setOnClickListener(this);
+            exerciseDescView.setOnClickListener(this);
+            exerciseMuscleView.setOnClickListener(this);
+            exerciseIDView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            // Get the position of the item that was clicked.
+            int mPosition = getLayoutPosition();
+            // Use that to access the affected item in mWordList.
+            String campus = mExerciseList[mPosition].getExerciseName();
+            Snackbar.make(exerciseNameView,
+                    campus + " clicked!",
+                    Snackbar.LENGTH_SHORT).show();
+
+        }
     }
 }
