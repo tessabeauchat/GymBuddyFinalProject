b800ba467f265e7c0f5c6fe7d3a34f0ee1288119
tessabeauchat <113142374+tessabeauchat@users.noreply.github.com>
2023-05-01 10:51:30 -0400

    Added menu navigation functionality to all current activities.

--- app/src/main/java/com/example/gymbuddy/ExerciseActivity.java
+++ app/src/main/java/com/example/gymbuddy/ExerciseActivity.java
@@ -1,7 +1,12 @@
 package com.example.gymbuddy;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -59,5 +64,30 @@ public class ExerciseActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int menuId = item.getItemId();
+
+        if (menuId == R.id.info_menu) {
+            Intent infoIntent = new Intent(this, InfoActivity.class);
+            startActivity(infoIntent);
+            return true;
+        } else if (menuId == R.id.exercise_menu) {
+            Log.d(TAG, "Exercise menu clicked!");
+            Intent exerciseIntent = new Intent(this, ExerciseActivity.class);
+            startActivity(exerciseIntent);
+            return true;
+        } else if (menuId == R.id.equipment_menu) {
+            Log.d(TAG, "Equipment menu clicked!");
+            Intent equipmentIntent = new Intent(this, EquipmentActivity.class);
+            startActivity(equipmentIntent);
+            return true;
+        } else if (menuId == R.id.workouts_menu) {
+            Intent workoutIntent = new Intent(this, WorkoutActivity.class);
+            startActivity(workoutIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
